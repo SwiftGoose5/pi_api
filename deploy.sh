efd4208e794347f0c48ee8da47a961649846409a
elliotgiese <elliot.giese@masses.social>
2025-11-06 13:53:32 -0500

    update dockerfile to restart always

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 docker build -t my-fastapi-app .
-docker stop fastapi-container
-docker rm fastapi-container
+docker stop fastapi-container 2>/dev/null || true
+docker rm fastapi-container 2>/dev/null || true
+
 docker run -d \
   --name fastapi-container \
+  --restart unless-stopped \
   -p 8000:8000 \
   --env-file ~/Documents/api_project/.env \
-  -v /sys/class/thermal:/sys/class/thermal:ro \
   -v ~/Documents/api_project/data.db:/app/data.db \
   my-fastapi-app
